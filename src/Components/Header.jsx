758956cc5bdad2ff422b5a5975c04de3b652b050
Muntasir Hassan <muntasirhassan17@gmail.com>
2024-12-31 01:59:16 +0600

    new update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <>
       <header className="text-center bg-[#00175f] text-slate-100 font-semibold py-2">
-        Get Best Domain Name{" "}
+        Get Best Domain and Hosting{" "}
         <a
           href="https://www.hostseba.com/aff.php?aff=1074"
           className="underline decoration-2 hover:text-red-600 duration-300"
--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,18 +1,23 @@
 function HeroSection() {
   return (
-    <div className="h-screen flex flex-col px-5 mt-10 sm:px-8 md:flex-row-reverse md:items-center md:mt-0 lg:mx-40 lg:px-0 ">
+    <div className="h-screen flex flex-col px-5 mt-10 sm:px-8 md:flex-row-reverse md:items-center md:mt-0 lg:mx-40 lg:px-0 max-sm:mb-8">
       <div className="md:w-2/4 flex justify-center items-center">
-        <img src="/download.png" className="w-full max-w-[450px]" alt="A men explaining which something"></img>
+        <img
+          src="/download.png"
+          className="w-full max-w-[450px]"
+          alt="A men explaining which something"
+        ></img>
       </div>
       <div className="flex flex-col gap-2 md:w-2/4">
-      <h1 className="text-white selection:bg-white">Buy domain and hosting in bangladesh with bkash</h1>
+        <h1 className="text-white selection:bg-white">
+          Buy domain and hosting in bangladesh with bkash
+        </h1>
         <h2 className="text-4xl font-sans lg:text-5xl xl:text-6xl">
-          Unlock Your Online Presence with the Perfect Domain Name
+          Get the Perfect Domain and Hosting
         </h2>
         <p className="font-serif lg:text-lg">
-          Find premium, memorable, and brandable domain names that make your
-          business stand out. Whether you're starting a new venture or expanding
-          your brand, we have the perfect domain for you.
+          Secure a premium domain and reliable hosting to boost your online
+          presence. Start or grow your brand with ease.
         </p>
         <a
           href="https://www.hostseba.com/aff.php?aff=1074"
