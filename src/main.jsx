9235f7ee607d01a4cf2efe111e60f605d0b5f087
Muntasir Hassan <muntasirhassan17@gmail.com>
2025-01-01 18:59:43 +0600

    performance update

--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,22 @@
-import React, { StrictMode, useEffect } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./Pages/Home/Home.jsx";
 import Blogs from "./Pages/Blogs/Blogs.jsx";
 import Blog1 from "./Pages/Blog 1/Blog1.jsx";
 
-// Google Analytics tracking hook
-const usePageTracking = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    if (window.gtag) {
-      window.gtag("config", "G-LCXTS90DWV", {
-        page_path: location.pathname,
-      });
-    }
-  }, [location]);
-};
-
-const App = () => {
-  usePageTracking();
-
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/blogs" element={<Blogs />} />
-      <Route
-        path="/blogs/how-to-choose-the-perfect-domain-name-for-your-business"
-        element={<Blog1 />}
-      />
-    </Routes>
-  );
-};
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route
+          path="/blogs/how-to-choose-the-perfect-domain-name-for-your-business"
+          element={<Blog1 />}
+        />
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
